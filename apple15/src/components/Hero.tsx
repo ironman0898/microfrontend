7a62de33a46f4d7707258f08415b82eba4c3653e
Abhi2708 <abhishek.junagade@caprium.com>
2024-08-14 15:37:41 +0530

    Creating structure for highlight video carousel.

--- apple15/src/components/Hero.tsx
+++ apple15/src/components/Hero.tsx
@@ -1,7 +1,54 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
 
-export default Hero
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState<string>(window.innerWidth < 750 ? smallHeroVideo : heroVideo);
+
+  const handleVideoSrc = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrc)
+
+    return ()=> {window.removeEventListener('resize', handleVideoSrc)}
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+
+    gsap.to('#cta', { opacity: 1, delay: 2, y:-50 })
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          { videoSrc && <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>}
+        </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+            <a href="#highlights" className="btn">Buy</a>
+            <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
